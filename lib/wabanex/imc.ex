88711bc4035ada103511aca088c8ac1a482aa784
Matheus Nunes <matheusnunesinfo@gmail.com>
2023-05-10 17:14:13 -0300

    calculadora imc

--- lib/wabanex/imc.ex
+++ lib/wabanex/imc.ex
@@ -7,9 +7,22 @@ defmodule Wabanex.IMC do
 
   defp handle_file({:ok, content}) do
     content
+    |> String.split("\n")
+    |> Enum.map(fn line -> parse_line(line) end)
+    |> Enum.into(%{})
   end
 
-  defp handle_file({:error, reason}) do
-    reason
+  defp handle_file({:error, _reason}) do
+    {:error, "Error while openning the file"}
   end
+
+  defp parse_line(line) do
+    line
+    |> String.split(",")
+    |> List.update_at(1, &String.to_float/1)
+    |> List.update_at(2, &String.to_float/1)
+    |> calculate_imc()
+  end
+
+  def calculate_imc([name, height, weight]), do: {name, weight / (height * height)}
 end
